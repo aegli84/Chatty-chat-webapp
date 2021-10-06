147710a4b64b92e11d2cf32c29b3c7b5f1064230
Andreea Egli - Hehl <64198045+aegli84@users.noreply.github.com>
2021-10-06 21:50:26 +0200

    v1

---
+++ src/GlobalStyles.js
@@ -0,0 +1,28 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+* {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+}
+    
+    html {
+        overflow: auto;
+        scroll-behavior: smooth;
+        @media (max-width: 1200px){
+            font-size: 75%;
+        }
+        ${'' /* height: auto; */}
+    }
+    
+    body {
+        ${'' /* height: auto; */}
+        width: 100%;
+        overflow-x: hidden;
+        overflow-y: hidden;
+        background-color: #BBC5BF;
+    }
+    
+`
+export default GlobalStyles;
